7a55a32e87b8e6ef5b83d284ee4e560d18f9db0d
󠀋౽ꫝⅈꪗᝰ <ovoaa@qq.com>
2025-10-26 10:02:02 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
-from astrbot.api.message_components import Video 
+from astrbot.api.message_components import Plain, Video 
 import aiohttp
+import base64
 
-@register("beauty_video", "美女视频", "获取美女视频的插件", "2.0")
+@register("beauty_video", "美女视频", "获取美女视频的插件", "1.0")
 class BeautyVideoPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
         self.api_url = "https://tx.2cnm.cn/video-api.php"  # 直接使用明文接口地址
         self.session = aiohttp.ClientSession()
-
+    
     async def terminate(self):
         await self.session.close()
 
     @filter.regex(r"^[/]?(美女视频|看美女)$")
     async def get_beauty_video(self, event: AstrMessageEvent):
         try:
-            # 直接使用接口地址作为视频源
-            video_component = Video.fromURL(self.api_url)
-            yield event.chain_result([video_component])
+            async with self.session.get(self.api_url) as response:
+                if response.status == 200:
+                    video_component = Video.fromURL(self.api_url)
+                    yield event.chain_result([video_component])
+                else:
+                    yield event.plain_result("获取视频失败 请稍后重试")
+
         except Exception:
-            yield event.plain_result("视频异常，请稍后重试")
+            yield event.plain_result("视频异常 请稍后重试")
